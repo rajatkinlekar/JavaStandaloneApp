2419072137ea58505e9cbf11a788b13bf0e34e20
rajatkinlekar <kinlekarrajat@gmail.com>
2024-11-17 03:10:47 +0530

    Second Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        pattern(5);
+        pattern(10);
     }
 
     public static void pattern(int n) {
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= i; j++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("* ");
             }
             System.out.print("\n");
